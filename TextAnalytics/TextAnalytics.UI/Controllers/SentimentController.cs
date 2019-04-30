6eda01d2650700b314e378b7393dedd678a08687
Cleiton Felipe de Moraes <Cleiton.De-Moraes@gft.com>
2019-04-30 15:57:23 -0300

    Update

---
+++ TextAnalytics/TextAnalytics.UI/Controllers/SentimentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TextAnalytics.Services;
+
+namespace TextAnalytics.UI.Controllers
+{
+    public class SentimentController : Controller
+    {
+        private SentimentService _service;
+        public SentimentController()
+        {
+            _service = new SentimentService();
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
